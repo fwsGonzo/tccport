04f9b59c9c7e4671f6ac72ac93525294c952af65
fwsgonzo <fwsgonzo@hotmail.com>
2017-01-25 23:31:01 +0100

    Remove dlfcn, remove networking

--- service.cpp
+++ service.cpp
@@ -16,39 +16,7 @@
 // limitations under the License.
 
 #include <os>
-#include <net/inet4>
 #include "tcc.hpp"
-
-extern "C"
-void* dlopen(const char *filename, int flag)
-{
-  printf("dlopen called for %s with flag %#x\n", filename, flag);
-  return nullptr;
-}
-extern "C"
-char* dlerror(void)
-{
-  printf("dlerror\n");
-  return nullptr;
-}
-extern "C"
-void* dlsym(void*, const char* symbol)
-{
-  printf("dlsym\n");
-  return nullptr;
-}
-extern "C"
-int   dlclose(void*)
-{
-  printf("dlclose\n");
-  return 0;
-}
-
-int execvp(const char *file, char *const argv[])
-{
-  printf("execvp called: %s\n", file);
-}
-
 
 void Service::start()
 {
@@ -57,35 +25,27 @@ void Service::start()
 
   // show that we are starting :)
   printf("*** " SERVICE_NAME " starting up...\n");
-  //_enable_heap_debugging_verbose(1);
 
-  // default configuration (with DHCP)
-  auto& inet = net::Inet4::ifconfig<>(10);
-  inet.network_config(
-      {  10, 0,  0, 42 },  // IP
-      { 255,255,255, 0 },  // Netmask
-      {  10, 0,  0,  1 },  // Gateway
-      {  10, 0,  0,  1 }); // DNS
-
-  /// TCC
   TCC tcc;
   tcc.add_sym("printf", (void*) printf);
   
   try {
+    // compile multiline string
     tcc.compile(R"HEYHEY(
-int printf(const char*, ...);
+      int printf(const char*, ...);
 
-void other_func(int param)
-{
-  printf("printf %d works too!!!!\n", param);
-}
+      void other_func(int param)
+      {
+        printf("printf %d works too!!!!\n", param);
+      }
 
-int test(int param)
-{
-  other_func(param);
-  return 666;
-}
+      int test(int param)
+      {
+        other_func(param);
+        return 666;
+      }
     )HEYHEY",
+    // compilation error handler
     [] (const char* msg) {
       printf("\n\t%s\n", msg);
     });
@@ -97,4 +57,9 @@ int test(int param)
   {
     printf("TCC: %s\n", err.what());
   }
+}
+
+int execvp(const char *file, char *const argv[])
+{
+  printf("execvp called: %s\n", file);
 }
